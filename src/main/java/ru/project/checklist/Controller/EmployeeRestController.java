80186cd4213da1503de424dac47c5fab156c1013
Branchaev Ivan <ivanbranchaev@bk.ru>
2019-04-16 22:25:09 +0400

    Stage 6

--- src/main/java/ru/project/checklist/Controller/EmployeeRestController.java
+++ src/main/java/ru/project/checklist/Controller/EmployeeRestController.java
@@ -1,13 +1,18 @@
 package ru.project.checklist.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.project.checklist.Entity.Employee;
 import ru.project.checklist.Service.EmployeeService;
 import ru.project.checklist.Service.PositionService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("rest")
@@ -16,28 +21,29 @@ public class EmployeeRestController {
     private EmployeeService emplService;
     @Autowired
     private PositionService positionService;
+
     @RequestMapping(value = "/employees",
             method = RequestMethod.GET,
-            produces = { MediaType.APPLICATION_JSON_VALUE})
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    public List<Employee> read(){
-        List<Employee> employees= (List<Employee>) emplService.read();
+    public List<Employee> read() {
+        List<Employee> employees = (List<Employee>) emplService.read();
         return employees;
     }
 
     @RequestMapping(value = "/employee",
             method = RequestMethod.POST,
-            produces = { MediaType.APPLICATION_JSON_VALUE})
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
     public Employee addEmployee(@RequestBody Employee emp,
                                 @RequestParam int managerId,
-                                @RequestParam int positionId){
-    return emplService.create(emp,managerId,positionId);
+                                @RequestParam int positionId) {
+        return emplService.create(emp, managerId, positionId);
     }
 
     @RequestMapping(value = "/employee",
             method = RequestMethod.PUT,
-            produces = { MediaType.APPLICATION_JSON_VALUE })
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
     public Employee update(@RequestBody Employee emp) {
 
@@ -46,10 +52,25 @@ public class EmployeeRestController {
 
     @RequestMapping(value = "/employees/{empNo}",
             method = RequestMethod.DELETE,
-            produces = { MediaType.APPLICATION_JSON_VALUE})
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
     public void deleteEmployee(@PathVariable("empNo") Integer empNo) {
         emplService.delete(empNo);
     }
 
+    @RequestMapping(value = "/employee/{id}/subordinates",
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public List<Employee> getEmployeeByManager(@PathVariable Integer id, HttpServletResponse response) throws IOException {
+        List<Employee> employees = (List<Employee>) emplService.read();
+        List<Employee> byManager = employees.stream()
+                .filter(x -> x.getManager() != null)
+                .filter(x -> x.getManager().getId().equals(id))
+                .collect(Collectors.toList());
+        if (byManager.isEmpty()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return byManager;
+    }
 }
